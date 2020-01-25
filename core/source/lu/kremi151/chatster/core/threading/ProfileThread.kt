76f0138357d74b0be2515020627c5c35692f0070
Michel Kremer <kremi151dev@gmail.com>
2020-01-25 17:41:23 +0100

    Basic profile structure

---
+++ core/source/lu/kremi151/chatster/core/threading/ProfileThread.kt
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2020 Michel Kremer (kremi151)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package lu.kremi151.chatster.core.threading
+
+import lu.kremi151.chatster.api.message.Message
+import lu.kremi151.chatster.api.profile.Profile
+
+class ProfileThread (
+        private val profile: Profile<out Message>,
+        private val context: ProfileContext
+): Thread() {
+
+    override fun run() {
+        try {
+            profile.listenForMessages(this::handleInboundMessage)
+            context.onShutdown(this, profile, null)
+        } catch (t: Throwable) {
+            context.onShutdown(this, profile, t)
+        }
+    }
+
+    private fun handleInboundMessage(message: Message) {
+        context.enqueueWorkerTask(Runnable {
+            context.handleMessage(message, profile)
+        })
+    }
+
+}
